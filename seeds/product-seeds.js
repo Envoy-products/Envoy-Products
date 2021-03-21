d98c4521942c847fe40c0053335bbbe066078b92
samiul1988 <samiulhaydereee@gmail.com>
2021-03-21 00:06:10 -0600

    updated product seeds function

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -1,35 +1,53 @@
-// const { Product } = require('../models');
+const { Product, ProductRet } = require('../models');
 
-// const productData = [
-//     {
-//         name:"Product 1", 
-//         description:"sample description", 
-//         website: "https://example.com", 
-//         product_img: "https://example.com",
-//         status:"pending",
-//         category_id: 1,
-//         retailerIds: [1, 2]
-//     },
-//     {
-//         name:"Product 2", 
-//         description:"sample description", 
-//         website: "https://example.com", 
-//         product_img: "https://example.com",
-//         status:"approved",
-//         category_id: 1,
-//         retailerIds: [1, 2]
-//     },
-//     {
-//         name:"Product 3", 
-//         description:"sample description", 
-//         website: "https://example.com", 
-//         product_img: "https://example.com",
-//         status:"featured",
-//         category_id: 1,
-//         retailerIds: [1, 2]
-//     } 
-// ];
+const productData = [
+    {
+        name:"Product 1", 
+        description:"sample description", 
+        website: "https://example.com", 
+        product_img: "https://example.com",
+        status:"pending",
+        category_id: 1,
+        user_id: 1,
+        retailerIds: [1, 2]
+    },
+    {
+        name:"Product 2", 
+        description:"sample description", 
+        website: "https://example.com", 
+        product_img: "https://example.com",
+        status:"approved",
+        category_id: 1,
+        user_id: 1,
+        retailerIds: [1, 2]
+    },
+    {
+        name:"Product 3", 
+        description:"sample description", 
+        website: "https://example.com", 
+        product_img: "https://example.com",
+        status:"featured",
+        category_id: 1,
+        user_id: 1,
+        retailerIds: [1, 2]
+    } 
+];
 
-// const seedProducts = () => Product.bulkCreate(productData);
+const seedProducts = async () => {
+    const productTableData = productData.map(({retailerIds, ...rest}) => ({...rest}));
+    const products = await Product.bulkCreate(productTableData);
+    
+    let productRetData = [];
+    products.map(product => {
+        const retailerIdsArr = productData[product.id - 1].retailerIds
+        if (retailerIdsArr.length) {
+            retailerIdsArr.map(retailer_id => productRetData.push({
+                product_id: product.id,
+                retailer_id,
+            }));
+        }
+    });
+    return ProductRet.bulkCreate(productRetData);
+}
 
-// module.exports = seedProducts;
+module.exports = seedProducts;

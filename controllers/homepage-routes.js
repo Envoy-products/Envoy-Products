f0e431ca86b2e7a1ff8c64fdf2f47c0a547ee2bc
Eric Normann <eric.n@me.com>
2021-03-24 13:44:10 -0400

    typo in homepage-routes

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -15,8 +15,7 @@ router.get('/', (req, res) => {
             'id', 
             'title', 
             'post_content', 
-            'post_url',
-
+            'post_url'
         ],
         include: [
             {

8c91c540f228dbbea42c8e5b69f0a9fd47c16cb2
Eric Normann <eric.n@me.com>
2021-03-28 16:08:24 -0400

    update server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: SESS_SECRET,
+    //secret: process.env.SESS_SECRET,
+    secret:"wahtevermano",
     cookie: {},
     resave: false,
     saveUninitialized: true,
